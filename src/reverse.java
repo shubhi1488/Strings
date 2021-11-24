7c419cb1626ec72403a59fa47ef6837c489b49df
HP <shubhi1488@gmail.com>
2021-11-24 22:49:30 +0530

    String Buffer

---
+++ src/reverse.java
@@ -0,0 +1,8 @@
+//reverses the current string through string buffer.
+public class reverse {
+    public static void main(String[] args) {
+        StringBuffer sb=new StringBuffer("shubhi");
+        sb.reverse();
+        System.out.println(sb);
+    }
+}//ihbuhs

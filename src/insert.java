5122231cb9847e6b15fc76242f9cea6b2ab2e533
HP <shubhi1488@gmail.com>
2021-11-24 21:02:32 +0530

    String Buffer

---
+++ src/insert.java
@@ -0,0 +1,7 @@
+public class insert {
+    public static void main(String[] args){
+        StringBuffer sb=new StringBuffer("hello");
+        sb.insert(1,"shubhi");
+        System.out.println(sb);
+    }
+}

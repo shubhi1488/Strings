927fbe58ac96a6f666379bb2711b8859eb5a6626
HP <shubhi1488@gmail.com>
2021-11-24 20:53:13 +0530

    String Buffer

---
+++ src/StringBufferExample.java
@@ -0,0 +1,8 @@
+//Appending new string to the original string using StringBuffer.....
+public class StringBufferExample {
+    public static void main(String[] args){
+        StringBuffer sb=new StringBuffer("Hello ");
+        sb.append("World");
+        System.out.println(sb);
+    }
+}
